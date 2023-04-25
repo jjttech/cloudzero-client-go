ef2662ac3d72d1270fda6aec7ca2da0a3be3efbf
Jonathan Thurman <jon@jjttechnology.com>
2023-04-24 19:46:55 -0700

    chore: update deps

--- cloudzero/option_test.go
+++ cloudzero/option_test.go
@@ -7,8 +7,7 @@ import (
 	"strings"
 	"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/jjttech/cloudzero-client-go/pkg/config"
--- internal/http/client_test.go
+++ internal/http/client_test.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	retryablehttp "github.com/hashicorp/go-retryablehttp"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 
